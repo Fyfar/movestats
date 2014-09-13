a2b7a3f665e8278cbfcf49a50202808329c33961
Yuriy Kucherenko <yuriy.kycherenko@gmail.com>
2014-09-13 15:56:36 +0300

    fix chart update

--- public/javascripts/server_req.js
+++ public/javascripts/server_req.js
@@ -1,61 +1,126 @@
 /**
  * Created by Fyfar on 01.09.2014.
  */
-function getProfile() {
+
+$(document).ready(function () {
+    var barChartData = {
+        labels: [],
+        datasets: [
+            {
+                fillColor: "rgba(0,200,88,0.8)",
+                strokeColor: "rgba(220,220,220,0.8)",
+                highlightFill: "rgba(0,200,88,1)",
+                highlightStroke: "rgba(220,220,220,1)",
+                data: []
+            }
+        ]
+    };
+    var ctx = $('#canvas-area').get(0).getContext("2d");
+    var chart = null;
+
+    function addToData(item, arr) {
+        if (item.steps !== undefined) {
+            barChartData.labels.push(numericToDate(arr.date, true));
+            barChartData.datasets[0].data.push(item.steps);
+        }
+    }
+
+    function addDataToChart(info, update) {
+        if (update) {
+            barChartData.labels = [];
+            barChartData.datasets[0].data = [];
+        }
+
+        var results = info;
+        for (var i = results.length - 1; i >= 0; i--) {
+            for (var summ_index in results[i].summary) {
+                addToData(results[i].summary[summ_index], results[i]);
+            }
+        }
+        if (update) {
+            chart.destroy();
+        }
+        document.getElementById('steps').innerHTML = sumSteps(info) + ' steps';
+        document.getElementById('distance').innerHTML = sumDistance(info) + ' meters';
+        chart = new Chart(ctx).Bar(barChartData);
+    }
+
+    function addDataToTables(info) {
+        var list = document.getElementById('list');
+        var result = info;
+
+        function addToList(item, arr) {
+            var sub_li1 = document.createElement('li');
+            var sub_li2 = document.createElement('li');
+            var sub_li3 = document.createElement('li');
+
+            sub_li1.appendChild(document.createTextNode(numericToDate(arr.date)));
+            sub_li2.appendChild(document.createTextNode(capitalize(item.activity)));
+            sub_li3.innerHTML = secondToHHMM(item.duration);
+            sub_li3.onmouseover = function () {
+                if (item.steps !== undefined) {
+                    this.innerHTML = '<span>' + item.steps + '</span>' + ' steps';
+                } else {
+                    this.innerHTML = '<span>' + item.distance + '</span>' + ' meters';
+                }
+            };
+            sub_li3.onmouseout = function () {
+                this.innerHTML = secondToHHMM(item.duration);
+            };
+
+            var sub_list = document.createElement('ul');
+            sub_list.className = 'sub-list';
+
+            sub_list.appendChild(sub_li1);
+            sub_list.appendChild(sub_li2);
+            sub_list.appendChild(sub_li3);
+
+            var li = document.createElement('li');
+            li.appendChild(sub_list);
+            list.appendChild(li);
+        }
+
+        var index = result.length;
+        for (var el_index in result) {
+            for (var summ_index in result[--index].summary) {
+                addToList(result[index].summary[summ_index], result[index]);
+            }
+        }
+    }
+
+    document.getElementById('month').onclick = function () {
+        getPastDaysInfo(31, function (info) {
+            addDataToChart(info, true);
+        });
+    };
+    document.getElementById('week').onclick = function () {
+        getPastDaysInfo(7, function (info) {
+            addDataToChart(info, true);
+        })
+    };
+
+    (function () {
+        getPastDaysInfo(7, function (info) {
+            addDataToTables(info.slice(-5), false);
+            addDataToChart(info, false);
+        });
+    })();
+});
+
+function getPastDaysInfo(countDays, callback) {
     var xhr = new XMLHttpRequest();
-    xhr.open('GET', '/api/profile/', true);
+
+    xhr.open('GET', '/api/summaries/past_days/' + countDays, true);
     xhr.onreadystatechange = function () {
         if (this.readyState != 4) return;
         if (this.status != 200) {
-            alert('Error ' + xhr.status + ' ' + xhr.statusText);
+            console.log('Error ' + xhr.status + ' ' + xhr.statusText);
             return;
         }
-        alert(xhr.responseText);
+
+        callback(JSON.parse(xhr.responseText));
     };
     xhr.send(null);
-}
-
-function addDataToTables(info) {
-    var list = document.getElementById('list');
-    var result = info;
-
-    function addToList(item, arr) {
-        var sub_li1 = document.createElement('li');
-        var sub_li2 = document.createElement('li');
-        var sub_li3 = document.createElement('li');
-
-        sub_li1.appendChild(document.createTextNode(numericToDate(arr.date)));
-        sub_li2.appendChild(document.createTextNode(capitalize(item.activity)));
-        sub_li3.innerHTML = secondToHHMM(item.duration);
-        sub_li3.onmouseover = function () {
-            if (item.steps !== undefined) {
-                this.innerHTML = '<span>' + item.steps + '</span>' + ' steps';
-            } else {
-                this.innerHTML = '<span>' + item.distance + '</span>' + ' meters';
-            }
-        };
-        sub_li3.onmouseout = function () {
-            this.innerHTML = secondToHHMM(item.duration);
-        };
-
-        var sub_list = document.createElement('ul');
-        sub_list.className = 'sub-list';
-
-        sub_list.appendChild(sub_li1);
-        sub_list.appendChild(sub_li2);
-        sub_list.appendChild(sub_li3);
-
-        var li = document.createElement('li');
-        li.appendChild(sub_list);
-        list.appendChild(li);
-    }
-
-    var index = result.length;
-    for (var el_index in result) {
-        for (var summ_index in result[--index].summary) {
-            addToList(result[index].summary[summ_index], result[index]);
-        }
-    }
 }
 
 function capitalize(string) {
@@ -93,60 +158,24 @@ function secondToHHMM(second) {
     return result;
 }
 
-function addDataToChart(info) {
-    var ctx = document.getElementById("canvas-area").getContext("2d");
-    var barChartData = {
-        labels: [],
-        datasets: [
-            {
-                fillColor: "rgba(0,200,88,0.8)",
-                strokeColor: "rgba(220,220,220,0.8)",
-                highlightFill: "rgba(0,200,88,1)",
-                highlightStroke: "rgba(220,220,220,1)",
-                data: []
-            }
-        ]
-    };
-
-    var results = info;
-
-    function addToChart(item, arr) {
-        if (item.steps !== undefined) {
-            barChartData.labels.push(numericToDate(arr.date, true));
-            barChartData.datasets[0].data.push(item.steps);
+function sumSteps(info) {
+    var steps = 0;
+    for (var i = 0; i <= info.length - 1; i++) {
+        if (info[i].summary === null) continue;
+        if (info[i].summary[0].steps !== undefined) {
+            steps += info[i].summary[0].steps;
         }
     }
+    return steps;
+}
 
-    var index = results.length;
-    for (var el_index in results) {
-        for (var summ_index in results[--index].summary) {
-            addToChart(results[index].summary[summ_index], results[index]);
+function sumDistance(info) {
+    var meters = 0;
+    for (var i = 0; i <= info.length - 1; i++) {
+        if (info[i].summary === null) continue;
+        if (info[i].summary[0].distance !== undefined) {
+            meters += info[i].summary[0].distance;
         }
     }
-    window.chart = new Chart(ctx).Bar(barChartData);
+    return meters;
 }
-
-function getPastDaysInfo(countDays, callback) {
-    var xhr = new XMLHttpRequest();
-
-    xhr.open('GET', '/api/summaries/past_days/' + countDays, true);
-    xhr.onreadystatechange = function () {
-        if (this.readyState != 4) return;
-        if (this.status != 200) {
-            console.log('Error ' + xhr.status + ' ' + xhr.statusText);
-            return;
-        }
-
-        callback(JSON.parse(xhr.responseText));
-    };
-    xhr.send(null);
-}
-
-function setInfo() {
-    getPastDaysInfo(10, function (info) {
-        addDataToTables(info.slice(-5));
-        addDataToChart(info);
-    });
-}
-
-window.onload = setInfo();
